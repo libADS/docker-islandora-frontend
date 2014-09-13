beb6168e0741154dac91a20e0e4d7a970c845079
Mark Cooper <markchristophercooper@gmail.com>
2014-09-12 21:18:21 -0700

    At least tmp kakadu url 'cus it works

--- get_binaries.sh
+++ get_binaries.sh
@@ -3,5 +3,5 @@
 wget -P bin/ -N https://account.jwplayer.com/static/download/jwplayer-6.8.zip
 wget -P bin/ -N http://www.videojs.com/downloads/video-js-4.0.0.zip
 wget -P bin/ -N http://projects.iq.harvard.edu/files/fits/files/fits-0.8.0.zip
-wget -P bin/ -N http://www.kakadusoftware.com/executables/KDU733_Demo_Apps_for_Linux-x86-64_140118.zip
+wget -P bin/ -N https://dl.dropboxusercontent.com/u/870845/apps/KDU733_Demo_Apps_for_Linux-x86-64_140118.zip
 wget -P bin/ -N http://openseadragon.github.io/releases/openseadragon-bin-0.9.129.zip
