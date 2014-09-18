4b7d937c7f71c9198d692ccd8c4bf0e6c28a5dc4
Mark Cooper <markchristophercooper@gmail.com>
2014-09-18 14:54:15 -0700

    Update run script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 8080:8080 --name fedora --link mysql:db islandora/frontend:latest
+docker run -d -p 80:80 --name islandora --link mysql:db --link fedora:backend --link djatoka:djatoka -v $ISLANDORA_SOURCE:/source islandora/frontend:latest
 
